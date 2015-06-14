23d8fc3c08f1a0c260b72fd85a91ecf630c1f93f
stevenratton <steven.ratton@gmail.com>
2015-06-15 00:21:28 +0200

    MAJ 15/06/2015
    Version 1

---
+++ js/afficher_cacher_div.js
@@ -0,0 +1,17 @@
+function afficher_cacher(id)
+{
+    var classID = '#'+id;
+    if(document.getElementById(id).style.visibility=="hidden")
+    {
+        $(classID).show();
+        document.getElementById(id).style.visibility="visible";
+        document.getElementById('bouton_'+id).innerHTML='Cacher le texte';
+    }
+    else
+    {
+        $(classID).hide();
+        document.getElementById(id).style.visibility="hidden";
+        document.getElementById('bouton_'+id).innerHTML='Afficher le texte';
+    }
+    return true;
+}
